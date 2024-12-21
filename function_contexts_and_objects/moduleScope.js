44e3d972d0e5108de1d6ca870a8bf08381159153
Derek Novak <derek.novak1@gmail.com>
2024-12-21 11:58:11 -0600

    Started Lesson 3

---
+++ function_contexts_and_objects/moduleScope.js
@@ -0,0 +1,18 @@
+// Use "node moduleScope.js" to run this code
+
+var foo = 'foo';
+// with `var`, the variable is in the module scope
+// and not added to the global object
+
+bar = 'bar';
+// without `var` declaration, the variable is in the global scope
+// and added to the global object
+
+let qux = 'qux';
+// with `let`, the variable is in the module scope
+// and is not added to the global object
+
+console.log(global.foo);    // => undefined
+console.log(global.bar);    // => bar
+console.log(global.qux);    // => undefined
+console.log(qux);           // => qux
